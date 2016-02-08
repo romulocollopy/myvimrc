f8929671a609be626cb2ca7003b8a17c66cadd10
Rômulo Collopy <romulocollopy@gmail.com>
2016-02-08 14:03:37 -0200

    install libs

--- install.sh
+++ install.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+if [!hash pip 2> /dev/null]; then
+    sudo pacman -S python2-pip python-pip --noconfirm
+fi
+
+if [!hash npm 2> /dev/null]; then
+    sudo pacman -S nodejs npm --noconfirm
+fi
+
 $PWD = `pwd`
 
 #Link .vimrc to reps file
@@ -7,13 +15,15 @@ ln -s `pwd`/vimrc ~/.vimrc
 
 # Install Vundle
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-
 # Install Vundle Plugins
 vim +PluginInstall +qall
+sudo pip2 install rope
+cd ~/.vim/bundle/ropevim/;sudo python2 setup.py install
 
 # Install cmake to compile YouCompleteMe
-sudo pacman -S cmake --noconfirm
-
+if [!hash cmake 2> /dev/null]; then
+    sudo pacman -S cmake --noconfirm
+fi
 # Build youCompleteMe
 cd ~/.vim/bundle/YouCompleteMe;
 if hash go 2> /dev/null; then
